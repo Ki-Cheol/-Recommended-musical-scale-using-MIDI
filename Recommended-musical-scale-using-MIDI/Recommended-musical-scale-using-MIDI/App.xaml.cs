e7639b2b2923223af9c95692d5fb5aa267b8ef78
pkc-PC\pkc <jja147@naver.com>
2017-05-22 16:29:32 +0900

    WPF생성

---
+++ Recommended-musical-scale-using-MIDI/Recommended-musical-scale-using-MIDI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Recommended_musical_scale_using_MIDI
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
